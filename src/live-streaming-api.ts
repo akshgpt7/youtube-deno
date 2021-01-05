c2a81f6b26f3be0c309bcf0a53d0e72d70530004
Michel Diz <MichelDiz@users.noreply.github.com>
2021-01-05 02:08:07 -0300

    remove repeated code

--- src/live-streaming-api.ts
+++ src/live-streaming-api.ts
@@ -2,28 +2,8 @@ import * as schemas from "./schemas.ts";
 import { YouTube } from "./youtube-deno.ts";
 
 export class YouTube_live extends YouTube {
-  key: string;
-  token: (string | boolean);
-  headers: schemas.header = {};
-  contentHeaders: schemas.header = {};
-
   constructor(readonly apiKey: string, accessToken: (boolean | string)) {
     super(apiKey, accessToken);
-    this.key = apiKey;
-    this.token = accessToken;
-    if (this.token == false) {
-      this.headers = {
-        "Accept": "application/json",
-      };
-    } else {
-      this.headers = {
-        "Authorization": `Bearer ${this.token}`,
-        "Accept": "application/json",
-      };
-    }
-
-    this.contentHeaders = this.headers;
-    this.contentHeaders["Content-Type"] = "application/json";
   }
 
   liveBroadcasts_list(params: schemas.schema_live_LiveBroadcasts_list) {
