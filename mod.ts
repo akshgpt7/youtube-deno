44de796b2bef9c8baf06e0cfaa048c91596d54b2
Aksh Gupta <akshgpt7@gmail.com>
2021-01-08 04:44:45 +0530

    Update the external mod.ts file

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
-export { YouTube } from "./src/youtube-deno.ts";
-export * from "./src/oauth.ts";
-export * from "./src/schemas.ts";
+export { YouTube } from "./youtube-deno.ts";
+export { YouTube_live } from "./live-streaming-api.ts";
+
+export * from "./oauth.ts";
+export * from "./schemas/schemas.ts";
